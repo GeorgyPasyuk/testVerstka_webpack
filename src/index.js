32610fa1d4602d5944cbea80b13a9698be2b1f50
GeorgyPasyuk <115622303+GeorgyPasyuk@users.noreply.github.com>
2023-02-14 23:02:17 +0300

    добавлена адаптивность

--- src/index.js
+++ src/index.js
@@ -2,4 +2,19 @@ import './index.html';
 import './index.scss';
 
 
+function formatInputs() {
+    const items = document.querySelectorAll('.item');
 
+    items.forEach(item => {
+        item.addEventListener('input', () => {
+            if (item.value.length > 19) {
+                item.value = item.value.slice(0, 19);
+            }
+            const inputValue = item.value.replace(/[^\d]/g, '');
+            const formattedValue = inputValue.replace(/(\d{3})(?=\d)/g, '$1 ');
+
+            item.value = formattedValue;
+        });
+    });
+}
+formatInputs()
