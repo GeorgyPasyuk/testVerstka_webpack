448b5fa651a833fa116595b1fea5a0b4ba42871f
GeorgyPasyuk <115622303+GeorgyPasyuk@users.noreply.github.com>
2023-02-15 00:45:07 +0300

    добавлен неполный функционал калькулятора, рефакторинг

---
+++ dist/assets/index.js
@@ -0,0 +1,15 @@
+import "./index.html";
+import "./index.scss";
+function formatInputs() {
+  const items = document.querySelectorAll(".item");
+  items.forEach(item => {
+    item.addEventListener("input", () => {
+      if (item.value.length > 19) {
+        item.value = item.value.slice(0, 19);
+      }
+      const inputValue = item.value.replace(/[^\d]/g, "");
+      item.value = inputValue.replace(/(\d{3})(?=\d)/g, "$1 ");
+    });
+  });
+}
+formatInputs();
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import "./index.html";
 import "./index.scss";
 
-function formatInputs() {
-  const items = document.querySelectorAll(".item");
+const slider = document.querySelector("#vznosSlider");
+const sliders = document.querySelectorAll(".slider");
+const items = document.querySelectorAll(".item");
+const percent = document.querySelector(".percents");
+const item = document.querySelector("#vznos");
+const stoimost = document.querySelector("#stoimost");
 
+function formatInputs() {
   items.forEach((item) => {
     item.addEventListener("input", () => {
       if (item.value.length > 19) {
@@ -15,3 +20,41 @@ function formatInputs() {
   });
 }
 formatInputs();
+
+function sliderControl() {
+  sliders.forEach((slider) => {
+    slider.addEventListener("input", (e) => {
+      const index = Array.from(sliders).indexOf(slider);
+      items[index].value = e.target.value;
+    });
+  });
+}
+sliderControl();
+
+function inputControl() {
+  items.forEach((item) => {
+    item.addEventListener("input", (e) => {
+      const index = Array.from(items).indexOf(item);
+      sliders[index].value = e.target.value;
+    });
+  });
+}
+
+inputControl();
+
+function percentControl() {
+  slider.addEventListener("input", (e) => {
+    percent.innerHTML = e.target.value + "%";
+  });
+}
+percentControl();
+
+function vznosControl() {
+  slider.addEventListener("input", (e) => {
+    const percent = e.target.value;
+    const value = (stoimost.value * percent) / 100;
+    item.value = value;
+  });
+}
+
+vznosControl();
