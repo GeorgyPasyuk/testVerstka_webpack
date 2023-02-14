4ec6a88d0a4050b6b342fb8bb36571eaf7e4f3db
GeorgyPasyuk <115622303+GeorgyPasyuk@users.noreply.github.com>
2023-02-14 23:51:40 +0300

    рефакторинг, добавлен файл README

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-import './index.html';
-import './index.scss';
-
+import "./index.html";
+import "./index.scss";
 
 function formatInputs() {
-    const items = document.querySelectorAll('.item');
+  const items = document.querySelectorAll(".item");
 
-    items.forEach(item => {
-        item.addEventListener('input', () => {
-            if (item.value.length > 19) {
-                item.value = item.value.slice(0, 19);
-            }
-            const inputValue = item.value.replace(/[^\d]/g, '');
-            const formattedValue = inputValue.replace(/(\d{3})(?=\d)/g, '$1 ');
-
-            item.value = formattedValue;
-        });
+  items.forEach((item) => {
+    item.addEventListener("input", () => {
+      if (item.value.length > 19) {
+        item.value = item.value.slice(0, 19);
+      }
+      const inputValue = item.value.replace(/[^\d]/g, "");
+      item.value = inputValue.replace(/(\d{3})(?=\d)/g, "$1 ");
     });
+  });
 }
-formatInputs()
+formatInputs();
